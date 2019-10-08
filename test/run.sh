7c44a73e824db368d0161d7f4fb7afc8854cd4b6
IMOKURI <nenegi.01mo@gmail.com>
2019-10-08 22:05:41 +0900

    update format

--- test/run.sh
+++ test/run.sh
@@ -10,11 +10,11 @@ VADER_OUTPUT_FILE=$(mktemp)
 trap "rm -f ${VADER_OUTPUT_FILE}" EXIT INT QUIT TERM
 
 if [[ ! -d "vader.vim" ]]; then
-  git clone https://github.com/junegunn/vader.vim.git
+    git clone https://github.com/junegunn/vader.vim.git
 fi
 
 # Open neovim with readonly mode just to execute all *.vader tests.
-${RUNVIM} -u minimal_init.vim -c 'Vader! case/**/*.vader' > /dev/null
+${RUNVIM} -u minimal_init.vim -c 'Vader! case/**/*.vader' >/dev/null
 VADER_EXIT=$?
 
 if [[ -n "${VADER_OUTPUT_FILE}" ]]; then
