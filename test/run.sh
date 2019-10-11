9297a8507acd69fccb631819e0661ebf0b622a91
IMOKURI <nenegi.01mo@gmail.com>
2019-10-11 13:20:58 +0900

    add coverage

--- test/run.sh
+++ test/run.sh
@@ -14,11 +14,13 @@ if [[ ! -d "vader.vim" ]]; then
 fi
 
 # Open neovim with readonly mode just to execute all *.vader tests.
-${RUNVIM} -u minimal_init.vim -c 'Vader! case/*.vader' >/dev/null
+covimerage run ${RUNVIM} -u minimal_init.vim -c 'Vader! case/*.vader' >/dev/null
 VADER_EXIT=$?
 
 if [[ -n "${VADER_OUTPUT_FILE}" ]]; then
     cat "${VADER_OUTPUT_FILE}"
+
+    covimerage xml
 fi
 
 exit ${VADER_EXIT}
